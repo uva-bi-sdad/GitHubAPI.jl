f34a404e7f9e42942a6edbac5e5b3abaa59a9259
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2019-08-21 15:23:50 -0400

    Fix runtests

---
+++ test/runtests.jl
@@ -0,0 +1,14 @@
+using Test, GitHubAPI
+
+@testset "Licenses" begin
+    @test isa(Vector{String}, get_licenses())
+end
+@testset "Search" begin
+    @test parse_repos!("postgresql")
+    @test parse_repos!("postgresql",
+                       "2019-10-29T14:37:16+00:00..2019-08-15T00:00:00-04:00")
+end
+@testset "Repository" begin
+    @test parse_commits!("uva-bi-sdad/GitHubAPI.jl")
+    @test parse_commits!("uva-bi-sdad/GitHubAPI.jl", "2018-08-12 17:29:38+00")
+end
